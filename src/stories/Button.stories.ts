c5c102c3daee95e6fa36016e18c64bd29f6b1c3d
mohaideenkhadar <126789643+mohaideenkhadar@users.noreply.github.com>
2025-02-22 17:07:36 +0530

    Update Button.stories.ts

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -2,11 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
 import { Button } from './Button';
+import { withDesign } from 'storybook-addon-designs';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
+  decorators: [withDesign]
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
