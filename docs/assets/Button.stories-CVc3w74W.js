5f9dd35679fdaac13d39db68888db08d2712a6cc
mohaideenkhadar <mohaideenabdulkhadar006@gmail.com>
2025-02-24 14:20:46 +0530

    Working docs folder

---
+++ docs/assets/Button.stories-CVc3w74W.js
@@ -0,0 +1,33 @@
+import{f as z}from"./index-iZ8NAl3x.js";import{B as I}from"./Button-Cm-dtDA9.js";import"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";const k={title:"Example/Button",component:I,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:z()}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}},o={args:{primary:!1,label:"Button"}},t={parameters:{design:{type:"figma",url:"https://www.figma.com/design/hxfh59XGYFrIPb1pljV9qO/Abdul---Portfolio?node-id=0-1&t=hd0SPxMzIsXxvtft-1"}}};var n,l,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var m,i,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,d,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var f,b,B;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(B=(b=s.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var y,S,h;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...(h=(S=o.parameters)==null?void 0:S.docs)==null?void 0:h.source}}};var x,w,P;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  //I have add the figma design url in Button.stories.ts
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/hxfh59XGYFrIPb1pljV9qO/Abdul---Portfolio?node-id=0-1&t=hd0SPxMzIsXxvtft-1"
+    }
+  }
+}`,...(P=(w=t.parameters)==null?void 0:w.docs)==null?void 0:P.source}}};const q=["Primary","Secondary","Large","Small","New","abdulDesign"];export{a as Large,o as New,r as Primary,e as Secondary,s as Small,q as __namedExportsOrder,t as abdulDesign,k as default};
---
+++ src/stories/Button.stories copy.ts
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
+
+export const New: Story = {
+  args: {
+    primary: false,
+    label: "Button",
+  },
+};
+
+export const abdulDesign = {  //I have add the figma design url in Button.stories.ts
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/hxfh59XGYFrIPb1pljV9qO/Abdul---Portfolio?node-id=0-1&t=hd0SPxMzIsXxvtft-1",
+    },
+  },
+};
